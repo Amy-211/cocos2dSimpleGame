df7e9032403179470a9288dbad4d8de78a7f8017
lingyun <apple@applematoiMac.local>
2012-04-12 15:45:13 +0800

    test
    test

---
+++ cocos2dTest/HelloWorldLayer.h
@@ -0,0 +1,21 @@
+//
+//  HelloWorldLayer.h
+//  cocos2dTest
+//
+//  Created by 辉 太郎 on 12-4-12.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ cocos2dTest/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  cocos2dTest
+//
+//  Created by 辉 太郎 on 12-4-12.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
